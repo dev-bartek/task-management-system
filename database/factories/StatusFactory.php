f48525f916a0dbfc556d4a5988e3db11deb8197e
Bartlomiej Przymus <162451053+dev-bartek@users.noreply.github.com>
2024-03-23 18:36:55 +0000

    feat: add Status model

---
+++ database/factories/StatusFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class StatusFactory extends Factory
+{
+    public function definition(): array
+    {
+        $statusName = fake()->words(2);
+
+        return [
+            'name' => $statusName,
+            'slug' => Str::slug($statusName),
+        ];
+    }
+}
