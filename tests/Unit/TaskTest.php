985af9a6c9d1b717847821fa49307ef5dd9be8c4
Bartlomiej Przymus <162451053+dev-bartek@users.noreply.github.com>
2024-03-25 19:02:05 +0000

    test: update test for task

--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Models\Task;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Carbon;
 
+uses(RefreshDatabase::class);
+
 it('can set task as completed', function () {
-    $task = Task::factory()->make();
+    $task = Task::factory()->for(User::factory())->make();
 
     expect($task->completed_at)->toBeNull();
 
@@ -14,7 +18,7 @@ it('can set task as completed', function () {
 });
 
 it('can check if task is completed by using method', function () {
-    $task = Task::factory()->make();
+    $task = Task::factory()->for(User::factory())->make();
 
     expect($task->isCompleted())->toBeFalse();
 
