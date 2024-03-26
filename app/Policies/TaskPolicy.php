fc1e150bd7ff25ca9242e6f70a32d1a240ef7c57
Bartlomiej Przymus <162451053+dev-bartek@users.noreply.github.com>
2024-03-26 15:39:19 +0000

    feat: update Task policy to allow admin user to edit users tasks

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -14,7 +14,7 @@ class TaskPolicy
 
     public function view(User $user, Task $task): bool
     {
-        return $task->user->getKey() === $user->getKey();
+        return $task->user->getKey() === $user->getKey() || $user->isAdmin();
     }
 
     public function create(User $user): bool
@@ -24,21 +24,21 @@ class TaskPolicy
 
     public function update(User $user, Task $task): bool
     {
-        return $task->user->getKey() === $user->getKey();
+        return $task->user->getKey() === $user->getKey() || $user->isAdmin();
     }
 
     public function delete(User $user, Task $task): bool
     {
-        return $task->user->getKey() === $user->getKey();
+        return $task->user->getKey() === $user->getKey() || $user->isAdmin();
     }
 
     public function restore(User $user, Task $task): bool
     {
-        return $task->user->getKey() === $user->getKey();
+        return $task->user->getKey() === $user->getKey() || $user->isAdmin();
     }
 
     public function forceDelete(User $user, Task $task): bool
     {
-        return $task->user->getKey() === $user->getKey();
+        return $task->user->getKey() === $user->getKey() || $user->isAdmin();
     }
 }
