e1cb1c5792573bb70fac5376798355df5fbaff7c
Bartlomiej Przymus <162451053+dev-bartek@users.noreply.github.com>
2024-03-26 12:15:53 +0000

    feat: prep enum for filament

--- app/Enums/TaskPriority.php
+++ app/Enums/TaskPriority.php
@@ -3,12 +3,28 @@
 namespace App\Enums;
 
 use App\Enums\Traits\EnumToArray;
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasLabel;
 
-enum TaskPriority: string
+enum TaskPriority: string implements HasLabel, HasColor
 {
     use EnumToArray;
 
     case Low = 'low';
     case Medium = 'medium';
     case High = 'high';
+
+    public function getLabel(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getColor(): string|array|null
+    {
+        return match ($this) {
+            self::Low => 'success',
+            self::Medium => 'warning',
+            self::High => 'danger',
+        };
+    }
 }
