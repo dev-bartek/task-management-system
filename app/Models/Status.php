fe15b9a55e825bddbdfa91927075006f3ae26533
Bartlomiej Przymus <162451053+dev-bartek@users.noreply.github.com>
2024-03-26 12:11:00 +0000

    refactor: added status model attribute to fillables and revert soft deletes

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -4,13 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Status extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
-    public function tasks()
+    protected $fillable = [
+        'name',
+    ];
+
+    public function tasks(): HasMany
     {
         return $this->hasMany(Task::class);
     }
